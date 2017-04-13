97585429d9a4f70fe49127cdd87922c237c18042
Rubi Cabral <rubisc@gmail.com>
2017-04-12 20:08:07 -0700

    commented out passport info

--- college_tracker/server.js
+++ college_tracker/server.js
@@ -5,12 +5,12 @@ var express   = require('express'),
 	cookieParser   = require('cookie-parser'),
 	bodyParser	= require('body-parser'),
 	mongoose    = require('mongoose'),
-	passport    = require('passport'),
+	// passport    = require('passport'),
 	port        = process.env.PORT || 3000,
 	studentRoutes  = require('./backend/config/student_routes.js'),
 	collegeRoutes  = require('./backend/config/college_routes.js')
 	// session     = require('express-session'),
-	bcrypt 			= require('bcrypt-nodejs')
+	// bcrypt 			= require('bcrypt-nodejs')
 
 // // mount cors as middleware like this:
 app.use(cors())
