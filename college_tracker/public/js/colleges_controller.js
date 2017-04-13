c2eab601fb63fc87d87d21b5f7db06a7ec010f20
Rubi Cabral <rubisc@gmail.com>
2017-04-13 14:34:44 -0700

    switched to heroku link

--- college_tracker/public/js/colleges_controller.js
+++ college_tracker/public/js/colleges_controller.js
@@ -16,8 +16,8 @@ function CollegesController($http){
   function getColleges(){
     $http
       // will need to change this to heroku when deploying
-      .get('http://localhost:3000/api/colleges')
-      // .get('https://college-tracker.herokuapp.com/api/colleges')
+      // .get('http://localhost:3000/api/colleges')
+      .get('https://college-tracker.herokuapp.com/api/colleges')
       .then(function(response) {
         vm.all = response.data
     })
