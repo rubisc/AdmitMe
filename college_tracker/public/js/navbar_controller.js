4e30afd80d83209509ce9fe686a8e06d86a0744b
Rubi Cabral <rubisc@gmail.com>
2017-05-03 17:09:31 -0700

    navbar has authorization now

---
+++ college_tracker/public/js/navbar_controller.js
@@ -0,0 +1,18 @@
+
+(function() {
+  "use strict";
+
+  angular
+    .module("college_tracker")
+    .controller("NavbarController", NavbarController);
+
+  NavbarController.$inject = ["$log", "authService"];
+
+  function NavbarController($log, authService) {
+    var vm = this;
+
+    vm.authService = authService;
+
+    $log.info("NavbarController loaded!");
+  }
+})();
